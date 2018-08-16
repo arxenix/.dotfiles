c2f34521429fc4e75b4855b5fd31499a852ad808
Ankur Sundara <me@ankursundara.com>
2018-08-16 12:17:05 -0700

    Create mac_defaults.sh

---
+++ mac_defaults.sh
@@ -0,0 +1,5 @@
+# show hidden files in finder
+defaults write com.apple.finder AppleShowAllFiles YES
+
+# disable .DS_STORE files
+defaults write com.apple.desktopservices DSDontWriteNetworkStores true
